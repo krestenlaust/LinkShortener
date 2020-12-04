c5b8d19d3545fe8761d54aae68c31134eb529b21
Kresten Laust <kressckerl@gmail.com>
2020-12-04 23:14:21 +0100

    fjerenede lodret streg

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_url(short_link: str) -> str:
 
 
 def generate_url():
-    return xeger(r'[llΙ|I∣।ǀІ𐌠ⅼ౹ꞁ𐌉❘]{8}')
+    return xeger(r'[llΙI∣।ǀІ𐌠ⅼ౹ꞁ𐌉❘]{8}')
 
 
 @app.route('/', methods=['GET', 'POST'])
